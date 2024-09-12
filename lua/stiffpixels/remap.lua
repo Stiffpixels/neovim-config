0cb5f55d0a644f951752e39865d08e77b04cf1b1
Stiffpixels <saifimuzammil2@gmail.com>
2024-09-12 22:22:50 +0530

    forgot to save some changes oops

--- lua/stiffpixels/remap.lua
+++ lua/stiffpixels/remap.lua
@@ -5,11 +5,31 @@ vim.keymap.set("n", "<leader>q", ":q<CR>")
 
 vim.keymap.set("n","<leader>pv", ":Ex<CR>")
 
+-- move entire selection by capital j or k
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- keep the cursor in the center while moving around
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+-- keep the current selection by pasting using leader + p
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- copy to the system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- delete into the system clipboard
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
